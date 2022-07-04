efddcf9f82fb884fcb9603706a36b1ceeb04ee82
marcb <marcbitenl@gmail.com>
2022-07-04 19:28:30 -0300

    Projeto criado

---
+++ README.md
@@ -0,0 +1,15 @@
+# Formação Spring Framework
+
+### Spring Boot API Rest: Segurança da API, Cache e Monitoramento
+
+### Tópicos abordados
+
+- Pagine e ordene os resultados da API
+- Melhore desempenho com Spring Cache
+- Protege a API com Spring Security e JWT
+- Monitore os endpoints com Spring Boot Actuator e Admin
+- Documente a API com Swagger
+
+# Certificado
+
+https://cursos.alura.com.br/certificate/23c069d5-17ee-4396-a973-b24fa37ac237
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.controller.form;
+
+import org.springframework.lang.NonNull;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+	@NonNull
+	private String titulo;
+
+	@NonNull
+	private String mensagem;
+
+	@NonNull
+	private String nomeCurso;
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+
+}
